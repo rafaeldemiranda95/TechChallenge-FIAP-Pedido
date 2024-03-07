912f1992a08324b771d5c97f376cb4db2d370383
AirtonPatrocinioJR <airtonpatrocinio.jr@gmail.com>
2024-03-06 22:19:43 -0300

    olha ai rafinha

--- app/Repository/ProdutoRepository.php
+++ app/Repository/ProdutoRepository.php
@@ -4,14 +4,29 @@ namespace App\Repository;
 
 use App\Models\Produto;
 use Illuminate\Http\Request;
+use Kreait\Firebase\Contract\Firestore;
+use Kreait\Laravel\Firebase\Facades\Firebase;
 
 class ProdutoRepository
 {
-
+    private $firestore;
+    private $database;
+    public function __construct(Firestore $firestore)
+    {
+        $this->firestore = $firestore;
+        $this->database = $this->firestore->database();
+    }
     public function listarProdutos()
     {
-        $produtos = Produto::all();
-        return response()->json(['data' => $produtos], 200);
+        $data = [
+            'nome' => 'João',
+            'idade' => 30,
+            'email' => 'joao@example.com'
+        ];
+
+        $this->database->collection('usuarios')->add($data);
+        // $produtos = Produto::all();
+        return response()->json(['data' => "Funciona"], 200);
     }
     public function listarProdutosId(string $id)
     {
