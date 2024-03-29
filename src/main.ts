02ba9ccedb84d5db0440dafe275b7645178a8f0c
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2024-03-29 08:43:24 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
